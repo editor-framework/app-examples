16bd1d0a2d197bce90e7667180ff7b0617847dab
Johnny Wu <KarasAya@gmail.com>
2016-06-17 19:42:42 +0800

    update

--- three-js-inspector/packages/inspector/panel/panel.js
+++ three-js-inspector/packages/inspector/panel/panel.js
@@ -21,21 +21,6 @@ Editor.Panel.extend({
     }
   `,
 
-  // template: `
-  //   <h2>Procedural</h2>
-  //   <div id="procedural"></div>
-
-  //   <h2>Predefine</h2>
-  //   <div id="predefine">
-  //     <ui-prop name="Name" type="string" value="Foobar"></ui-prop>
-  //     <ui-prop name="Content" type="string" value="Hello\nWorld" multiline></ui-prop>
-  //     <ui-prop name="ID" type="number" value="2000"></ui-prop>
-  //     <ui-prop name="Enable" type="boolean" value="true"></ui-prop>
-  //     <ui-prop name="Info" type="object" value='{"foo": "foo", "bar": "bar", "foobar": 20}'></ui-prop>
-  //     <ui-prop name="Color" type="color" value="#09f"></ui-prop>
-  //   </div>
-  // `,
-
   template: `
     <div id="view" class="flex-1">
     </div>
@@ -48,19 +33,6 @@ Editor.Panel.extend({
   },
 
   ready () {
-    // this.inspect([
-    //   { name: 'name', type: 'string', value: 'Foobar' },
-    //   { name: 'content', type: 'string', value: 'Hello\nWorld', attrs: { multiline: true } },
-    //   { name: 'id', type: 'number', value: 2000 },
-    //   { name: 'enable', type: 'boolean', value: true },
-    //   { name: 'info', type: 'object', value: [
-    //     { name: 'foo', type: 'string', value: 'foo' },
-    //     { name: 'bar', type: 'string', value: 'bar' },
-    //     { name: 'foobar', type: 'number', value: 20 },
-    //   ]},
-    //   { name: 'color', type: 'color', value: '#09f' },
-    // ]);
-
     Editor.Ipc.sendToPanel('scene', 'scene:query-dump', (err, data) => {
       if ( !err ) {
         this.inspect(data);
--- work-with-vue/packages/vue-panels/panel/builtin-ui.js
+++ work-with-vue/packages/vue-panels/panel/builtin-ui.js
@@ -15,12 +15,29 @@ Editor.Panel.extend({
   template: `
     <h2>Work with builtin ui</h2>
 
-    <ui-prop name="foo" type="string" :value="message" v-on:change="onChange"></ui-prop>
-    <ui-prop name="bar" type="string" :value="message" readonly></ui-prop>
+    <ui-prop name="foo 01" type="string" :value="message" v-on:change="onChange"></ui-prop>
+    <ui-prop name="foo 02" type="string" :value="message" readonly></ui-prop>
+    <ui-prop name="foo 03" type="string" v-model="message"></ui-prop>
     <p>Input Value = <span>{{message}}</span></p>
   `,
 
   ready () {
+    window.Vue.directive('model', {
+      twoWay: true,
+      bind () {
+        this.handler = event => {
+          this.set(event.detail.value);
+        };
+        this.el.addEventListener('change', this.handler);
+      },
+      unbind () {
+        this.el.removeEventListener('change', this.handler);
+      },
+      update (newValue) {
+        this.el.value = newValue;
+      },
+    });
+
     new window.Vue({
       el: this.shadowRoot,
 
