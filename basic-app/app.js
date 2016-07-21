ff4000bafc280a907849c92c440c989f133de460
Anthony Erlinger <aerlinger@gmail.com>
2016-07-21 02:58:07 -0400

    Update files in basic-app

--- basic-app/app.js
+++ basic-app/app.js
@@ -1,24 +1,32 @@
 'use strict';
 
+/**
+ * Entry point for our application.
+ * *Note that this file is the "main" file in package.json!*
+ */
+
 const Editor = require('../../index');
 
 Editor.App.extend({
 
   /**
    * init is called once Editor Framework has completed its internal initialization.
-   * Here, you may want to define any application-level configuration or behavior. 
-   * 
-   * @param opts An object representing the command line options used to start this Application 
-   * @param callback function that signals the parent process that initialization is complete. 
+   * Here, you may want to define any application-level configuration or behavior.
+   *
+   * @param opts An object representing the command line options used to start this Application
+   * @param callback function that signals the parent process that initialization is complete.
    *    You'll want to call manually this within your `init` function once you're ready for the application
    *    to run
    */
-  init ( opts, callback ) {
+  init (opts, callback) {
     Editor.init({
+      'package-search-path': [
+        Editor.url('app://package-doesnt-exist/')
+      ]
       // An object defining application-level configuration options for initialization will go here  
     });
 
-    
+
     Editor.success("main.js: call to init() completed successfully")
     // You'll need to call this directly to signal Editor-Framework that you're ready to run the application
     callback();
@@ -33,15 +41,15 @@ Editor.App.extend({
      *   - An object defining the parameters of the window
      */
     let mainWin = new Editor.Window('main', {
-      title: 'Editor Framework Basic Example',
-      width: 900,
-      height: 700,
-      minWidth: 900,
+      title:     'Editor Framework Basic Example',
+      width:     900,
+      height:    700,
+      minWidth:  900,
       minHeight: 700,
-      show: false,
+      show:      false,
       resizable: true,
     });
-    
+
     // Tell Editor that that is our root window.
     Editor.Window.main = mainWin;
 
@@ -49,19 +57,19 @@ Editor.App.extend({
     mainWin.restorePositionAndSize();
 
     // page-level test case
-    mainWin.load( 'app://index.html' );
+    mainWin.load('app://index.html');
 
     // load and show main window
     mainWin.show();
 
     // open dev tools if needed
-    if ( Editor.argv.showDevtools ) {
+    if (Editor.argv.showDevtools) {
       // NOTE: open dev-tools before did-finish-load will make it insert an unused <style> in page-level
-      mainWin.nativeWin.webContents.once('did-finish-load', function () {
+      mainWin.nativeWin.webContents.once('did-finish-load', function() {
         mainWin.openDevTools();
       });
     }
-    
+
     mainWin.focus();
 
     Editor.success("main.js: call to run() completed successfully")
