767417e8e920347199604f9177f20b35b04e19fb
Anthony Erlinger <aerlinger@gmail.com>
2016-08-04 10:10:31 -0400

    Minor fix/update to webpack config

--- work-with-react/packages/react-wrapper/webpack.config.base.js
+++ work-with-react/packages/react-wrapper/webpack.config.base.js
@@ -11,7 +11,7 @@ let baseConfig = {
     ]
   },
   output:    {
-    path:          './panel/dist',
+    path:          require("path").resolve('./panel/dist'),
     filename:      'bundle.js',
     libraryTarget: 'commonjs2'
   },
