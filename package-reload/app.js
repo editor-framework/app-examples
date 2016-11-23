dd927675df6baa040561ec651102da509a563375
zhangbin <zhangbin@cocos2d-x.org>
2016-11-23 18:33:33 +0800

    Rename the app demo of reload package.

---
+++ package-reload/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Editor = require('../../index');
+
+Editor.App.extend({
+  init ( opts, cb ) {
+    Editor.init({
+      'package-search-path': [
+        'app://packages/',
+      ],
+    });
+
+    cb ();
+  },
+
+  run () {
+    // create main window
+    Editor.run('app://index.html', {
+      title: 'Editor Framework',
+      width: 900,
+      height: 700,
+      minWidth: 900,
+      minHeight: 700,
+      show: false,
+      resizable: true,
+    });
+  },
+});
--- reload-package-rules/packages/simple-package/i18n/zh.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    title: '简易插件'
-};
